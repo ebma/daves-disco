4f37c6a1d777cad3fedfdaab8f7d848a363497cb
marcel <marcel@wolfgang-ebert.de>
2019-08-29 21:00:43 +0200

    Add volume-up and volume-down commands

---
+++ src/commands/volume-up.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "v-up",
+  description: "Increases the volume by 5 units.",
+  aliases: ["mamalauda"],
+  usage: "[command]",
+  cooldown: 5,
+  execute(message) {
+    const musicBot = message.client.music.bot;
+    const volumeCommand = musicBot.volumeFunction;
+
+    if (musicBot.queues.size > 0) {
+      const currentVolume = musicBot.queues.get(message.guild.id).volume;
+      const newVolume = currentVolume + 5;
+
+      volumeCommand(message, newVolume);
+    } else {
+      message.channel.send(
+        `You have to start playing a song before configuring the volume.`
+      );
+    }
+  }
+};
