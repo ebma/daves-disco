e016268bc57499c1084a548b596177a0699a73de
marcel <marcel@wolfgang-ebert.de>
2019-09-10 11:56:18 +0200

    Add queue command

---
+++ src/commands/music/queue.ts
@@ -0,0 +1,34 @@
+import { Message } from "discord.js"
+import { Command } from "discord-akairo"
+import MusicPlayerManager from "../../libs/MusicPlayerManager"
+import _ from "lodash"
+import { createEmbedForTracks } from "../../libs/util/embeds"
+
+class QueueCommand extends Command {
+  constructor() {
+    super("queue", {
+      aliases: ["queue", "list", "next-songs"],
+      channelRestriction: "guild"
+    })
+  }
+
+  async exec(message: Message, args: any) {
+    const musicPlayer = MusicPlayerManager.getPlayerFor(message.guild.id)
+
+    const tracks: Track[] = []
+    musicPlayer.queuedTracks.forEach(track => {
+      tracks.push(track)
+    })
+
+    if (tracks.length === 0){
+      return message.reply("I've nothing to play... :flushed:")
+    } else {
+      const trackEmbed = createEmbedForTracks(tracks)
+      trackEmbed.setTitle("These are my next tracks:")
+      return message.channel.send(trackEmbed)
+    }
+
+  }
+}
+
+export default QueueCommand
