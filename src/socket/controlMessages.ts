45c2d9d066429e9ea312544fc2ff07d8ecbe9770
marcel <marcel@wolfgang-ebert.de>
2019-09-28 10:31:27 +0200

    Filter and sort guilds and users before responding to control message

--- src/socket/controlMessages.ts
+++ src/socket/controlMessages.ts
@@ -19,9 +19,12 @@ const handleControlMessages = (socket: Socket, client: AkairoClient) => (data: C
   switch (data.type) {
     case "getGuilds":
       const guilds = client.guilds
-      const reducedGuilds = guilds.map(g => {
-        return { id: g.id, name: g.name }
-      })
+      const reducedGuilds = guilds
+        .map(g => {
+          return { id: g.id, name: g.name }
+        })
+        .sort((a, b) => a.name.localeCompare(b.name))
+
       sendControlResult(reducedGuilds)
       break
     case "getUsers":
@@ -31,9 +34,12 @@ const handleControlMessages = (socket: Socket, client: AkairoClient) => (data: C
         const guild = client.guilds.find(g => g.id === data.guildID)
         if (guild) {
           const members = guild.members
-          const reducedMembers = members.map(member => {
-            return { id: member.id, name: member.displayName }
-          })
+          const reducedMembers = members
+            .filter(member => !member.user.bot)
+            .map(member => {
+              return { id: member.id, name: member.displayName }
+            })
+            .sort((a, b) => a.name.localeCompare(b.name))
           sendControlResult(reducedMembers)
         } else {
           sendControlError(`Could not find guild with ID ${data.guildID}`)
