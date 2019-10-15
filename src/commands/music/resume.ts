fb6743c91f215bf372134dfac3dec3fc80486e8d
marcel <marcel@wolfgang-ebert.de>
2019-10-15 10:14:39 +0200

    Add pause and resume commands

---
+++ src/commands/music/resume.ts
@@ -0,0 +1,46 @@
+import { Message, TextChannel } from "discord.js"
+import { Command } from "discord-akairo"
+import _ from "lodash"
+import MusicPlayerManager from "../../libs/MusicPlayerManager"
+import { CommandMessage } from "../../typings/exported-types"
+
+class ResumeCommand extends Command {
+  constructor() {
+    super("resume", {
+      aliases: ["resume"],
+      channelRestriction: "guild"
+    })
+  }
+
+  async exec(message: Message, args: any) {
+    const musicPlayer = MusicPlayerManager.getPlayerFor(message.guild.id)
+
+    const error = await musicPlayer.resumeStream()
+    return message.reply(error ? error : "Resuming...")
+  }
+
+  executeSilent(args: CommandMessage) {
+    return new Promise<void>((resolve, reject) => {
+      const { guildID, userID } = args
+      const guild = this.client.guilds.find(g => g.id === guildID)
+      const member = guild.members.find(m => m.id === userID)
+      const textChannel = guild.channels.find(
+        channel => channel.name === "general" && channel.type === "text"
+      ) as TextChannel
+
+      const musicPlayer = MusicPlayerManager.getPlayerFor(guildID)
+
+      try {
+        const error = musicPlayer.resumeStream()
+        if (!error) {
+          textChannel.send(`${member} wants to keep going.`)
+        }
+        resolve()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+}
+
+export default ResumeCommand
