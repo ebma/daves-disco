ca481a722a609ca1f64fdc6f4bce9cf5e6a44a0e
Marcel Ebert <mail@marcel-ebert.de>
2019-12-20 19:50:22 +0100

    TODO change me

--- src/libs/util/streams.ts
+++ src/libs/util/streams.ts
@@ -1,32 +1,13 @@
 import { Readable } from "stream"
 import * as ytdl from "ytdl-core"
-import { createTracksFromSearchTerm } from "../../shared/util/youtube"
 import _ from "lodash"
-import { trackError } from "../../shared/util/trackError"
 
-export async function createTrackStream(track: Track, callback: (stream: Readable) => void) {
-  try {
-    if (track.source === "spotify" && (track as SpotifyTrack).initialized === false) {
-      await initalizeSpotifyTrack(track as SpotifyTrack)
-    }
-
-    const trackInfo = await ytdl.getInfo(track.url)
-    callback(
-      ytdl
-        .downloadFromInfo(trackInfo, { quality: "highestaudio", filter: "audioonly" })
-        .on("error", err => trackError(err))
-    )
-  } catch (error) {
-    trackError(error, this)
-  }
-}
-
-async function initalizeSpotifyTrack(spotifyTrack: SpotifyTrack) {
-  const ytTracks = await createTracksFromSearchTerm(
-    `${spotifyTrack.title} - ${spotifyTrack.source === "spotify" ? spotifyTrack.artists : undefined}`,
-    1
-  )
-  const ytTrack = ytTracks[0]
-  spotifyTrack.url = ytTrack.url
-  spotifyTrack.initialized = true
-}
+// async function initalizeSpotifyTrack(spotifyTrack: SpotifyTrack) {
+//   const ytTracks = await createTracksFromSearchTerm(
+//     `${spotifyTrack.title} - ${spotifyTrack.source === "spotify" ? spotifyTrack.artists : undefined}`,
+//     1
+//   )
+//   const ytTrack = ytTracks[0]
+//   spotifyTrack.url = ytTrack.url
+//   spotifyTrack.initialized = true
+// }
