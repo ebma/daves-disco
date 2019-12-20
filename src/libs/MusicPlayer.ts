def13f1c43a1a321c14ab9808003751bb13390d6
Marcel Ebert <mail@marcel-ebert.de>
2019-12-20 19:49:08 +0100

    Reimplement MusicPlayer and add test

--- src/libs/MusicPlayer.ts
+++ src/libs/MusicPlayer.ts
@@ -1,195 +1,171 @@
-import { Queue } from "typescript-collections"
-import { VoiceConnection } from "discord.js"
-import { VoiceChannel } from "discord.js"
 import _ from "lodash"
-import { Message } from "discord.js"
-import { TextChannel } from "discord.js"
 import { setTimeout } from "timers"
+import { Subject, PartialObserver, Subscription } from "rxjs"
 import { trackError } from "../shared/util/trackError"
-import MessageSender from "../socket/MessageSender"
-import { createTrackStream } from "./util/streams"
 import ObservableQueue from "./ObservableQueue"
-import { RichEmbed } from "discord.js"
+import StreamManager, { Options, Dispatcher } from "./StreamManager"
+import { Readable } from "stream"
 
-export class MusicPlayer {
-  cachedMessage?: Message
-  currentTrack?: Track
-  voiceConnection: VoiceConnection
+export interface SubjectMessage {
+  messageType: "status" | "info" | "error" | "debug"
+  message: string
+  data?: any
+}
+
+export interface Channel {
+  full: boolean
+  join(): Promise<{ playStream: (stream: Readable, options?: Options) => Dispatcher }>
+}
+
+class MusicPlayer {
   queue: ObservableQueue<Track>
-  private volume: number = 0.1
   private currentDisconnectionTimeout: NodeJS.Timeout
+  private streamManager: StreamManager
+  private subject: Subject<SubjectMessage>
 
-  constructor() {
+  constructor(channel: Channel) {
     this.queue = new ObservableQueue<Track>()
+    this.subject = new Subject<SubjectMessage>()
 
-    this.queue.subscribe((currentTrack, remainingTracks) => {
-      this.currentTrack = currentTrack
-      if (this.isStreaming() === false && !this.isPaused()) {
-        this.createStream()
-      }
-      MessageSender.sendMessage("currentSong", currentTrack)
-      MessageSender.sendMessage("currentQueue", remainingTracks)
-    })
+    channel
+      .join()
+      .then(voiceConnection => {
+        this.streamManager = new StreamManager(voiceConnection.playStream)
+      })
+      .then(() => {
+        this.queue.subscribe((currentTrack, remainingTracks) => {
+          if (currentTrack && this.streamManager.playing === false && !this.paused) {
+            this.startStreaming(currentTrack)
+          }
+          this.subject.next({ messageType: "status", message: "currentSong", data: currentTrack })
+          this.subject.next({ messageType: "status", message: "currentQueue", data: remainingTracks })
+        })
+      })
+      .catch(error => this.subject.next({ messageType: "error", message: error }))
   }
 
-  isStreaming() {
-    return this.isInVoiceChannel() && !_.isNil(this.voiceConnection.dispatcher)
+  get paused() {
+    return this.streamManager.paused
   }
 
-  getVolume() {
-    return this.volume * 100
-  }
-
-  setVolume(newVol: number): string {
-    if (newVol < 0 || newVol > 100) {
-      return "Invalid volume! Choose a number between 0-100%"
-    }
-    this.volume = newVol / 100
-    if (this.isStreaming()) {
-      this.voiceConnection.dispatcher.setVolume(this.volume)
-    }
-    MessageSender.sendMessage("volume", newVol)
+  get playing() {
+    return this.streamManager.playing
   }
 
   get queuedTracks() {
     return this.queue.getRemaining()
   }
 
-  get channel() {
-    return _.isNil(this.cachedMessage) ? null : (this.cachedMessage.channel as TextChannel)
+  get currentTrack() {
+    return this.queue.getCurrent()
   }
 
-  async enqueue(item: Track) {
+  get volume() {
+    return this.streamManager.getVolume()
+  }
+
+  setVolume(newVol: number) {
+    try {
+      this.streamManager.setVolume(newVol)
+    } catch (error) {
+      this.subject.next({ messageType: "error", message: error })
+    }
+  }
+
+  enqueue(item: Track) {
     this.queue.addElement(item)
   }
 
-  async clear() {
+  clear() {
     this.queue.clear()
   }
 
-  async join(voiceChannel: VoiceChannel) {
-    if (voiceChannel.full) {
-      throw new Error("Voice channel is full!")
+  pauseStream() {
+    try {
+      this.streamManager.pause()
+      this.subject.next({ messageType: "status", message: "paused" })
+    } catch (error) {
+      this.subject.next({ messageType: "error", message: error })
     }
-    this.voiceConnection = await voiceChannel.join()
   }
 
-  async setMessage(message: Message) {
-    this.cachedMessage = message
-    this.cachedMessage.react("🤘")
+  resumeStream() {
+    try {
+      this.streamManager.resume()
+      this.subject.next({ messageType: "status", message: "resumed" })
+    } catch (error) {
+      this.subject.next({ messageType: "error", message: error })
+    }
   }
 
-  async pauseStream() {
-    if (!this.isStreaming()) return "I am not playing anything!"
-    else if (this.isPaused()) return "I am already paused!"
-    this.voiceConnection.dispatcher.pause()
-    MessageSender.sendMessage("paused")
-  }
-
-  async resumeStream() {
-    if (!this.isStreaming()) return "There is nothing to resume!"
-    else if (!this.isPaused()) return "I have already started playing!"
-    this.voiceConnection.dispatcher.resume()
-    MessageSender.sendMessage("resumed")
+  stopStream() {
+    this.subject.next({ messageType: "info", message: `Stopping stream.` })
+    this.streamManager.stopPlaying()
+    this.streamManager.disconnect()
   }
 
   shuffle() {
     this.queue.shuffle()
   }
 
-  skipCurrentSong() {
-    this.queue.moveForward()
-    if (this.isStreaming()) {
-      this.voiceConnection.dispatcher.end("skipped")
-      return true
-    } else {
-      return false
-    }
+  skipForward(amount: number = 1) {
+    this.streamManager.endCurrentSong()
+    this.queue.moveForward(amount)
   }
 
-  skipPrevious() {
-    this.queue.moveBack()
-    if (this.isStreaming()) {
-      this.voiceConnection.dispatcher.end("skipped")
-      return true
-    } else {
-      return false
-    }
+  skipPrevious(amount: number = 1) {
+    this.streamManager.endCurrentSong()
+    this.queue.moveBack(amount)
   }
 
-  async skipNextSongInQueue() {
-    return this.queue.moveForward()
-  }
-
-  stopStream() {
-    if (!this.isInVoiceChannel()) return "I am not connected to a voice channel!"
-    else if (this.isStreaming()) this.voiceConnection.dispatcher.end("forceStop")
-    this.voiceConnection.disconnect()
-    this.voiceConnection = null
-  }
-
-  async close(reason: string) {
-    this.trySendMessageToChannel(reason ? reason : "**Closing music session due to inactivity**")
-    this.stopStream()
-  }
-
-  disconnectIfReasonable() {
+  private disconnectIfReasonable() {
     if (this.queue && this.queue.size() === 0) {
-      this.trySendMessageToChannel("That's it for now... Later bitches! :metal:")
-      this.voiceConnection.disconnect()
-      this.voiceConnection = null
+      this.subject.next({ messageType: "info", message: `That's it for now... Later bitches! :metal:` })
+      this.streamManager.disconnect()
     }
   }
 
-  private isInVoiceChannel() {
-    return !_.isNil(this.voiceConnection)
-  }
+  private async startStreaming(track: Track) {
+    try {
+      const dispatcher = await this.streamManager.playTrack(track)
+      dispatcher
+        .once("start", () => {
+          this.subject.next({ messageType: "status", message: "playing" })
+          this.subject.next({
+            messageType: "info",
+            message: `:raised_hands: Let me see your hands while I play *${track.title}* :raised_hands:`
+          })
+        })
+        .once("end", reason => {
+          this.subject.next({ messageType: "info", message: `Played: *${track.title}*` })
 
-  private isPaused() {
-    return this.isStreaming() && this.voiceConnection.dispatcher.paused
-  }
-
-  private createStream() {
-    if (!this.currentTrack) return
-    createTrackStream(this.currentTrack, stream => {
-      const playingTrack = this.currentTrack
-      this.voiceConnection.playStream(stream, { seek: 0, volume: this.volume, passes: 1 })
-      this.voiceConnection.dispatcher.once("start", () => {
-        this.trySendMessageToChannel(
-          `:raised_hands: Let me see your hands while I play *${playingTrack.title}* :raised_hands: `
-        )
-      })
-      this.voiceConnection.dispatcher.once("end", reason => {
-        stream.destroy()
-        this.trySendMessageToChannel(`Played: *${playingTrack.title}*`)
-        if (reason !== "forceStop") {
-          if (this.voiceConnection.channel.members.every(member => member.deaf || member.user.bot)) {
-            this.trySendMessageToChannel("Stopping stream since no one is listening")
-            this.voiceConnection.disconnect()
-          } else if (this.queue.getRemaining().length > 0) {
-            return setTimeout(() => this.createStream(), 50)
-          } else if (this.queue.size() === 0) {
-            this.currentTrack = undefined
+          if (this.queue.size() === 0) {
             if (!this.currentDisconnectionTimeout) {
               this.currentDisconnectionTimeout = setTimeout(this.disconnectIfReasonable, 1000 * 60 * 15)
             }
             this.currentDisconnectionTimeout.refresh()
           }
-        }
-      })
-      this.voiceConnection.dispatcher.on("error", e => {
-        trackError(e, this)
-        this.trySendMessageToChannel(`I don't feel so good... (${e})`)
-      })
-    })
-    return "Music stream started"
-  }
 
-  public trySendMessageToChannel(message: string | RichEmbed, fallbackChannel?: TextChannel) {
-    if (this.cachedMessage) {
-      this.cachedMessage.channel.send(message)
-    } else if (fallbackChannel) {
-      fallbackChannel.send(message)
+          if (reason !== "forceStop") {
+            if (this.queue.size() > 0) {
+              this.queue.moveForward()
+            }
+          }
+        })
+        .on("error", e => {
+          trackError(e, "MusicPlayer.startStreaming on-dispatcher-error")
+          this.subject.next({ messageType: "error", message: e.message })
+        })
+    } catch (error) {
+      const errorMessage = `Could not start stream for track '${track.title}: ${error}`
+      trackError(errorMessage, "MusicPlayer.startStreaming try-catch-error")
+      this.subject.next({ messageType: "error", message: errorMessage })
     }
   }
+
+  subscribe(observer: PartialObserver<SubjectMessage>): Subscription {
+    return this.subject.subscribe(observer)
+  }
 }
+
+export default MusicPlayer
