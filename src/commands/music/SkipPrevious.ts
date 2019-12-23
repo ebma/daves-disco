74d239e62fd3cf292332795d03cb92d96b8227f9
Marcel Ebert <mail@marcel-ebert.de>
2019-12-23 11:21:09 +0100

    Make skipping multiple songs backwards possible

--- src/commands/music/SkipPrevious.ts
+++ src/commands/music/SkipPrevious.ts
@@ -1,3 +1,4 @@
+import _ from "lodash"
 import { trackError } from "../../shared/util/trackError"
 import { MusicCommand } from "./MusicCommand"
 
@@ -5,13 +6,33 @@ class SkipPrevious extends MusicCommand {
   constructor() {
     super("skip-previous", {
       aliases: ["skip-previous"],
+      args: [
+        {
+          id: "data",
+          default: 1,
+          type: word => {
+            if (!word || _.isNaN(word)) return null
+            const num = parseInt(word, 10)
+            if (num < 1) return null
+            return num
+          },
+          match: "content",
+          prompt: {
+            optional: true,
+            retry: "You must skip at least 1 song.."
+          }
+        }
+      ],
       channelRestriction: "guild"
     })
   }
 
-  async execute() {
+  async execute(args: { data: any }) {
+    const suppliedAmount = (args && args.data) || 1
+
     try {
-      this.musicPlayer.skipPrevious()
+      this.musicPlayer.skipPrevious(suppliedAmount)
+      return this.sendMessageToChannel(`Skipped ${suppliedAmount} songs backwards!`)
     } catch (error) {
       trackError(error, "SkipPrevious.execute")
       return this.sendMessageToChannel(`Could not skip to previous song: ${error}`)
