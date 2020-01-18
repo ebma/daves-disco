d98926c1eb57437e4ac3ea0c3888a39b76143db8
Marcel Ebert <mail@marcel-ebert.de>
2020-01-18 11:59:43 +0100

    Restyle QueueArea

--- web/src/components/Controls/Player/QueueArea.tsx
+++ web/src/components/Controls/Player/QueueArea.tsx
@@ -5,15 +5,53 @@ import ListItemText from "@material-ui/core/ListItemText"
 import ExpandLessIcon from "@material-ui/icons/ExpandLess"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 import Typography from "@material-ui/core/Typography"
-import Container from "@material-ui/core/Container"
 import Grid from "@material-ui/core/Grid"
-import { Link } from "@material-ui/core"
+import { Avatar, Link, ListItemAvatar, makeStyles, Paper } from "@material-ui/core"
 import { trackError } from "../../../shared/util/trackError"
 import { SocketContext } from "../../../context/socket"
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: 16,
+    marginBottom: 16,
+    padding: 16
+  },
+  queueItem: {
+    position: "relative",
+    padding: "16px 24px",
+  }
+}))
+
+interface QueueItemProps {
+  track: Track
+}
+
+function QueueItem(props: QueueItemProps) {
+  const classes = useStyles()
+  const { track } = props
+
+  return (
+    <ListItem button className={classes.queueItem} onClick={() => window.open(track.url, "_blank")}>
+      <ListItemAvatar>
+        <Avatar alt="thumbnail" src={track.thumbnail} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={track.title}
+        secondary={
+          <Link href={track.url} color="inherit" target="_blank" rel="noreferrer">
+            {track.url}
+          </Link>
+        }
+      />
+    </ListItem>
+  )
+}
 
 interface Props {}
 
 function QueueArea(props: Props) {
+  const classes = useStyles()
+
   const { addListener, connectionState, guildID, sendControlMessage } = React.useContext(SocketContext)
 
   const [currentQueue, setCurrentQueue] = React.useState<Track[]>([])
@@ -37,8 +75,18 @@ function QueueArea(props: Props) {
     setShow(!show)
   }, [show, setShow])
 
+  const listContent = React.useMemo(() => {
+    return currentQueue.length > 0 ? (
+      currentQueue.map((track, index) => <QueueItem track={track} key={index}></QueueItem>)
+    ) : (
+      <ListItem onClick={() => undefined} key={0}>
+        <ListItemText primary="No songs in queue..." />
+      </ListItem>
+    )
+  }, [currentQueue])
+
   return (
-    <Container style={{ marginTop: 16, marginBottom: 16 }}>
+    <Paper className={classes.paper}>
       <Grid container justify="space-between" onClick={toggleShow}>
         <Grid item>
           <Typography color="inherit" variant="h5">
@@ -50,25 +98,8 @@ function QueueArea(props: Props) {
         </Grid>
       </Grid>
 
-      {show ? (
-        <List style={{ flexGrow: 1 }}>
-          {currentQueue.map((track, index) => (
-            <ListItem onClick={() => undefined} key={index}>
-              <ListItemText
-                primary={track.title}
-                secondary={
-                  <Link href={track.url} color="inherit" target="_blank" rel="noreferrer">
-                    {track.url}
-                  </Link>
-                }
-              />
-            </ListItem>
-          ))}
-        </List>
-      ) : (
-        undefined
-      )}
-    </Container>
+      {show ? <List style={{ flexGrow: 1 }}>{listContent}</List> : undefined}
+    </Paper>
   )
 }
 
