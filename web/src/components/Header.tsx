1f9835c6457813f44304742f6d1e6fd21c6529aa
Marcel Ebert <mail@marcel-ebert.de>
2020-03-11 17:43:16 +0100

    Change Header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,14 +1,11 @@
 import React from "react"
-import Avatar from "@material-ui/core/Avatar"
-import Grid from "@material-ui/core/Grid"
-import Headset from "@material-ui/icons/Headset"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
+import Box from "@material-ui/core/Box"
 
 const useHeaderStyles = makeStyles(theme => ({
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+  typography: {
+    padding: theme.spacing(3)
   }
 }))
 
@@ -16,23 +13,11 @@ function Header() {
   const classes = useHeaderStyles()
 
   return (
-    <Grid container direction="row" justify="center" alignItems="center" spacing={5} style={{ margin: "auto" }}>
-      <Grid item>
-        <Avatar className={classes.avatar}>
-          <Headset />
-        </Avatar>
-      </Grid>
-      <Grid item>
-        <Typography variant="h2" align="center" color="primary">
-          Discord Music Bot
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Avatar className={classes.avatar}>
-          <Headset />
-        </Avatar>
-      </Grid>
-    </Grid>
+    <Box >
+      <Typography className={classes.typography} variant="h2" align="center" color="secondary">
+        Discord Music Bot
+      </Typography>
+    </Box>
   )
 }
 
