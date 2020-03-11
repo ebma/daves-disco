ba074d9bdaab3fe7cc16aec1a4d319b095e7215c
Marcel Ebert <mail@marcel-ebert.de>
2020-03-11 17:52:39 +0100

    Change max width of CurrentSongCard

--- web/src/components/Controls/Player/CurrentSongCard.tsx
+++ web/src/components/Controls/Player/CurrentSongCard.tsx
@@ -3,19 +3,24 @@ import Card from "@material-ui/core/Card"
 import CardActionArea from "@material-ui/core/CardActionArea"
 import CardContent from "@material-ui/core/CardContent"
 import CardMedia from "@material-ui/core/CardMedia"
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button"
 import CardActions from "@material-ui/core/CardActions"
 
-const useSongCardStyles = makeStyles({
-  card: {
-    maxWidth: 345
-  },
-  media: {
-    minWidth: 300
-  }
-})
+const useSongCardStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      maxWidth: 345,
+      [theme.breakpoints.down("xs")]: {
+        maxWidth: "65vw"
+      }
+    },
+    media: {
+      minWidth: 200
+    }
+  })
+)
 
 interface Props {
   currentTrack?: Track
