47e2760e620cb05542339a6fcb9d7edefb8c5a4e
Marcel Ebert <mail@marcel-ebert.de>
2020-03-11 16:21:35 +0100

    Change Footer design

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,9 +1,8 @@
 import React from "react"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
-import Container from "@material-ui/core/Container"
 import Link from "@material-ui/core/Link"
-import Box from "@material-ui/core/Box"
+import Container from "@material-ui/core/Container"
 
 function Copyright() {
   return (
@@ -20,9 +19,8 @@ function Copyright() {
 
 const useStyles = makeStyles(theme => ({
   footer: {
-    padding: theme.spacing(2),
     marginTop: "auto",
-    backgroundColor: "white"
+    padding: theme.spacing(2),
   }
 }))
 
@@ -30,12 +28,10 @@ export default function Footer() {
   const classes = useStyles()
 
   return (
-    <Box mt={8}>
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Copyright />
-        </Container>
-      </footer>
-    </Box>
+    <footer className={classes.footer}>
+      <Container maxWidth="sm">
+        <Copyright />
+      </Container>
+    </footer>
   )
 }
