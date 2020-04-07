2f83e294347878a30d54d5213438f6d3d94129b5
Marcel Ebert <mail@marcel-ebert.de>
2020-04-07 16:49:41 +0200

    Add guilds route and controller

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,18 +1,22 @@
 import cors from "cors"
 import express from "express"
 require("express-async-errors")
-import playlistsRouter from "./controllers/playlists"
-import tracksRouter from "./controllers/tracks"
-import { createLoginRouter } from "./controllers/login"
-import middleware from "./middleware"
 import { connect } from "../db/connection"
 import config from "../utils/config"
 import { MyClient } from "../bot/MyClient"
+import middleware from "./middleware"
+import { createGuildRouter } from "./controllers/guilds"
+import { createLoginRouter } from "./controllers/login"
+import playlistsRouter from "./controllers/playlists"
+import tracksRouter from "./controllers/tracks"
 
 connect(config.MONGODB_URI)
 
 export function initApp(client: MyClient) {
   const app = express()
+
+  const guildRouter = createGuildRouter(client)
+  const loginRouter = createLoginRouter(client)
 
   app.use(cors())
   app.use(express.json())
@@ -20,9 +24,8 @@ export function initApp(client: MyClient) {
 
   app.use("/api/playlists", playlistsRouter)
   app.use("/api/tracks", tracksRouter)
-
-  const loginRouter = createLoginRouter(client)
   app.use("/api/login", loginRouter)
+  app.use("/api/guilds", guildRouter)
 
   app.use(middleware.unknownEndpoint)
   app.use(middleware.errorHandler)
