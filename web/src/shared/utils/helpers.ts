a366ce03abc00fff41d46a56c4288c9f4819b064
Marcel Ebert <mail@marcel-ebert.de>
2020-04-07 13:26:02 +0200

    Add helpers for youtube and spotify

---
+++ web/src/shared/utils/helpers.ts
@@ -0,0 +1,47 @@
+import ytdl from "ytdl-core"
+
+function describesYoutubePlaylist(term: string): boolean {
+  const regex = /(^|\s)(https?:\/\/)?(www\.)?youtube\.com\/(playlist)\?(list)=([^\s&]+)[^\s]*($|\s)/g
+  const valid = term.match(regex)
+  return valid ? true : false
+}
+
+function isYoutubeVideo(url: string): boolean {
+  return ytdl.validateURL(url)
+}
+
+function isYoutubePlaylist(playlist: Playlist): playlist is YoutubePlaylist {
+  return playlist.source === "youtube"
+}
+
+export const YoutubeHelper = {
+  describesYoutubePlaylist,
+  isYoutubeVideo,
+  isYoutubePlaylist
+}
+
+function isSpotifyPlaylistURI(term: string) {
+  const regex = /spotify:playlist:[\d\w]+/g
+  return term.match(regex)
+}
+
+function isSpotifyTrack(track: Track): track is SpotifyTrack {
+  return track.source === "spotify"
+}
+
+function isSpotifyPlaylist(playlist: Playlist): playlist is SpotifyPlaylist {
+  return playlist.source === "spotify"
+}
+
+function getIDFromUri(uri: string) {
+  const split = uri.split(":")
+  const id = split[split.length - 1]
+  return id
+}
+
+export const SpotifyHelper = {
+  isSpotifyTrack,
+  isSpotifyPlaylist,
+  isSpotifyPlaylistURI,
+  getIDFromUri
+}
