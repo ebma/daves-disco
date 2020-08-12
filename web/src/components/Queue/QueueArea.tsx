967fea0dc494ce1fe0898e45cbe2af916b5745db
Marcel Ebert <mail@marcel-ebert.de>
2020-08-12 17:39:28 +0200

    Make QueueHeader mobile friendly

--- web/src/components/Queue/QueueArea.tsx
+++ web/src/components/Queue/QueueArea.tsx
@@ -1,5 +1,6 @@
 import Box from "@material-ui/core/Box"
 import Button from "@material-ui/core/Button"
+import { makeStyles } from "@material-ui/core/styles"
 import Tooltip from "@material-ui/core/Tooltip"
 import Typography from "@material-ui/core/Typography"
 import ClearIcon from "@material-ui/icons/Clear"
@@ -13,15 +14,50 @@ import { AppDispatch } from "../../app/store"
 import { clearTracks, shuffleTracks, updateLoopState } from "../../redux/playerSlice"
 import QueueList from "./QueueList"
 
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: 16
+  },
+  container: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 8,
+      marginBottom: 8
+    }
+  },
+  buttonBox: {
+    display: "flex",
+
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "center",
+      flexWrap: "wrap"
+    }
+  },
+  headerTitle: {
+    marginLeft: 16,
+    marginTop: 16
+  }
+}))
+
 interface QueueHeaderProps {
   onClearClick: () => void
   onShuffleClick: () => void
   onLoopClick: () => void
+  showButtons: boolean
   loopState: LoopState
 }
 
 function QueueHeader(props: QueueHeaderProps) {
-  const { onClearClick, onShuffleClick, onLoopClick, loopState } = props
+  const { onClearClick, onShuffleClick, onLoopClick, loopState, showButtons } = props
+  const classes = useStyles()
 
   const loopTooltipTitle =
     loopState === "none" ? "Don't repeat" : loopState === "repeat-one" ? "Repeat Track" : "Repeat"
@@ -36,35 +72,48 @@ function QueueHeader(props: QueueHeaderProps) {
     )
 
   return (
-    <Box display="flex">
-      <Tooltip arrow placement="top" title="Clear">
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={onClearClick}
-          startIcon={<ClearIcon />}
-          style={{ margin: 16 }}
-        >
-          Clear
-        </Button>
-      </Tooltip>
-      <Tooltip arrow placement="top" title="Shuffle">
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={onShuffleClick}
-          startIcon={<ShuffleIcon />}
-          style={{ margin: 16 }}
-        >
-          Shuffle
-        </Button>
-      </Tooltip>
-      <Tooltip arrow placement="top" title={loopTooltipTitle}>
-        <Button variant="contained" color="secondary" onClick={onLoopClick} startIcon={loopIcon} style={{ margin: 16 }}>
-          Repeat
-        </Button>
-      </Tooltip>
-    </Box>
+    <div className={classes.container}>
+      <Typography className={classes.headerTitle} color="textPrimary" variant="h3">
+        Queue
+      </Typography>
+      {showButtons && (
+        <Box className={classes.buttonBox}>
+          <Tooltip arrow placement="top" title="Clear">
+            <Button
+              className={classes.button}
+              color="secondary"
+              onClick={onClearClick}
+              startIcon={<ClearIcon />}
+              variant="contained"
+            >
+              Clear
+            </Button>
+          </Tooltip>
+          <Tooltip arrow placement="top" title="Shuffle">
+            <Button
+              className={classes.button}
+              color="secondary"
+              onClick={onShuffleClick}
+              startIcon={<ShuffleIcon />}
+              variant="contained"
+            >
+              Shuffle
+            </Button>
+          </Tooltip>
+          <Tooltip arrow placement="top" title={loopTooltipTitle}>
+            <Button
+              className={classes.button}
+              color="secondary"
+              onClick={onLoopClick}
+              startIcon={loopIcon}
+              variant="contained"
+            >
+              Repeat
+            </Button>
+          </Tooltip>
+        </Box>
+      )}
+    </div>
   )
 }
 
@@ -95,19 +144,13 @@ function QueueArea() {
 
   return (
     <>
-      <div style={{ alignItems: "center", display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-        <Typography color="textPrimary" variant="h3" style={{ marginLeft: 16, marginTop: 16 }}>
-          Queue
-        </Typography>
-        {queue.length > 0 && (
-          <QueueHeader
-            onClearClick={clear}
-            onShuffleClick={shuffle}
-            onLoopClick={switchLoopState}
-            loopState={loopState}
-          />
-        )}
-      </div>
+      <QueueHeader
+        onClearClick={clear}
+        onShuffleClick={shuffle}
+        onLoopClick={switchLoopState}
+        loopState={loopState}
+        showButtons={queue.length > 0}
+      />
       <QueueList />
     </>
   )
