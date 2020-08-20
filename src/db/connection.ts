e699a0979e9045ff46d21dacbe8df2bdd9b0c97b
Marcel Ebert <mail@marcel-ebert.de>
2020-08-20 18:57:23 +0200

    Add additional mongoose parameter

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,12 @@
 import mongoose from "mongoose"
 
 export function connect(databasePath: string) {
-  mongoose.connect(databasePath, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  mongoose.connect(databasePath, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
 
   const db = mongoose.connection
   db.on("error", console.error.bind(console, "connection error:"))
