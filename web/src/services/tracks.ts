2f1ae93d75f046db930bedd6b76089729e4e7619
Marcel Ebert <mail@marcel-ebert.de>
2020-10-25 10:36:34 +0100

    Split getList request into chunks to prevent crash

--- web/src/services/tracks.ts
+++ web/src/services/tracks.ts
@@ -2,6 +2,19 @@ import axios from "./axios-client"
 
 const baseUrl = "/api/tracks"
 
+function chunkArray(myArray: any[], chunk_size: number) {
+  var index = 0
+  var arrayLength = myArray.length
+  var tempArray = []
+
+  for (index = 0; index < arrayLength; index += chunk_size) {
+    const myChunk = myArray.slice(index, index + chunk_size)
+    tempArray.push(myChunk)
+  }
+
+  return tempArray
+}
+
 const getAll = async (guildID?: GuildID, limit: number = 20, order: string = "desc"): Promise<TrackModel[]> => {
   const response = await axios.get<TrackModel[]>(baseUrl, {
     params: {
@@ -24,12 +37,20 @@ const getFavourites = async (guildID?: GuildID): Promise<TrackModel[]> => {
 }
 
 const getList = async (trackIDs: Array<string>): Promise<TrackModel[]> => {
-  const response = await axios.get<TrackModel[]>(baseUrl + "/list", {
-    params: {
-      tracks: trackIDs
-    }
-  })
-  return response.data
+  const chunks = chunkArray(trackIDs, 100)
+  const promises = chunks.map(trackIDs =>
+    axios.get<TrackModel[]>(baseUrl + "/list", {
+      params: {
+        tracks: trackIDs
+      }
+    })
+  )
+
+  const response = await Promise.all(promises)
+  const data = response.reduce<TrackModel[]>((prev, current) => {
+    return prev.concat(current.data)
+  }, [])
+  return data
 }
 
 const getTrack = async (trackID: string): Promise<TrackModel> => {
