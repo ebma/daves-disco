54feb1024d9ea05f77e196d3832f542dd5aaa615
Marcel Ebert <mail@marcel-ebert.de>
2021-01-24 10:14:19 +0100

    Remove parts of redux state

--- web/src/redux/soundboardsSlice.ts
+++ web/src/redux/soundboardsSlice.ts
@@ -1,103 +1,12 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { AppThunk } from "../app/store"
-import soundboardService from "../services/soundboards"
 import { Messages } from "../shared/ipc"
-import { subscribeToMessages, sendMessage } from "./socketSlice"
-
-export interface SoundboardState {
-  error: string | null
-  items: SoundboardItemModel[]
-}
-
-const initialState: SoundboardState = {
-  error: null,
-  items: []
-}
-
-const soundboardSlice = createSlice({
-  name: "tracks",
-  initialState,
-  reducers: {
-    setError(state, action: PayloadAction<string | null>) {
-      state.error = action.payload
-    },
-    setItem(state, action: PayloadAction<SoundboardItemModel>) {
-      const newItem = action.payload
-      const foundIndex = state.items.findIndex(item => item._id === newItem._id)
-      if (foundIndex !== -1) {
-        state.items[foundIndex] = newItem
-      } else {
-        state.items.push(newItem)
-      }
-    },
-    setItems(state, action: PayloadAction<SoundboardItemModel[]>) {
-      state.items = action.payload
-    },
-    removeItem(state, action: PayloadAction<SoundboardItemModel>) {
-      const filtered = state.items.filter(item => item._id !== action.payload._id)
-      state.items = filtered
-    }
-  }
-})
-
-export const { setError, setItem, setItems, removeItem } = soundboardSlice.actions
-
-export default soundboardSlice.reducer
-
-export const fetchItems = (): AppThunk<Promise<SoundboardItemModel[]>> => async (dispatch, getState) => {
-  const { user } = getState().user
-  try {
-    const items = await soundboardService.getAll(user?.guildID)
-    dispatch(setItems(items))
-    return items
-  } catch (error) {
-    dispatch(setError(error.message))
-    return Promise.reject(error)
-  }
-}
-
-export const subscribeItems = (): AppThunk<UnsubscribeFn> => (dispatch, getState) => {
-  const { user } = getState().user
-  if (user) {
-    const unsubscribeItemsChange = dispatch(
-      subscribeToMessages(Messages.SoundboardItemsChange, () => dispatch(fetchItems()))
-    )
-
-    return () => unsubscribeItemsChange
-  } else {
-    return () => undefined
-  }
-}
+import { sendMessage } from "./socketSlice"
 
 export const playSound = (source: string, volume: number): AppThunk<Promise<void>> => async (dispatch, getState) => {
   const { user } = getState().user
   if (user) {
     return dispatch(sendMessage(Messages.PlaySound, source, volume)).catch(error => {
-      dispatch(setError(error.message))
-      throw error
+      console.error(error)
     })
-  } else {
-    dispatch(setError("User not available"))
-  }
-}
-
-export const updateItem = (item: SoundboardItemModel): AppThunk<Promise<void>> => async (dispatch, getState) => {
-  const updatedItem = await soundboardService.updateItem(item._id, item)
-  dispatch(setItem(updatedItem))
-}
-
-export const deleteItem = (item: SoundboardItemModel): AppThunk<Promise<void>> => async (dispatch, getState) => {
-  await soundboardService.deleteItem(item._id)
-  dispatch(removeItem(item))
-}
-
-export const createItem = (item: { name: string; source: string }): AppThunk<Promise<void>> => async (
-  dispatch,
-  getState
-) => {
-  const { user } = getState().user
-  if (user) {
-    const createdItem = await soundboardService.createItem({ ...item, guild: user.guildID })
-    dispatch(setItem(createdItem))
   }
 }
--- web/src/redux/tracksSlice.ts
+++ web/src/redux/tracksSlice.ts
@@ -1,101 +1,18 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import _ from "lodash"
 import { AppThunk } from "../app/store"
-import TrackService from "../services/tracks"
+import { TrackFieldsFragment } from "../services/graphql/graphql"
 import youtubeService from "../services/youtube"
 import { Messages } from "../shared/ipc"
-import { sendMessage, subscribeToMessages } from "./socketSlice"
+import { sendMessage } from "./socketSlice"
 
-export interface TracksState {
-  error: string | null
-  tracks: TrackModel[]
-}
-
-const initialState: TracksState = {
-  error: null,
-  tracks: []
-}
-
-const tracksSlice = createSlice({
-  name: "tracks",
-  initialState,
-  reducers: {
-    setError(state, action: PayloadAction<string | null>) {
-      state.error = action.payload
-    },
-    setTrack(state, action: PayloadAction<TrackModel>) {
-      const newTrack = action.payload
-      const foundIndex = state.tracks.findIndex(track => track._id === newTrack._id)
-      if (foundIndex !== -1) {
-        state.tracks[foundIndex] = newTrack
-      } else {
-        state.tracks.push(newTrack)
-      }
-    },
-    setTracks(state, action: PayloadAction<TrackModel[]>) {
-      state.tracks = action.payload
-    }
-  }
-})
-
-export const { setError, setTrack, setTracks } = tracksSlice.actions
-
-export default tracksSlice.reducer
-
-export const fetchTracks = (): AppThunk<Promise<TrackModel[]>> => async (dispatch, getState) => {
-  const { user } = getState().user
-  try {
-    const recents = await TrackService.getAll(user?.guildID, 20, "desc")
-    const favourites = await TrackService.getFavourites(user?.guildID)
-    const tracks = recents.concat(favourites)
-    const dedup = _.uniqBy(tracks, trackModel => trackModel._id)
-    dispatch(setTracks(dedup))
-    return tracks
-  } catch (error) {
-    dispatch(setError(error.message))
-    return Promise.reject(error)
-  }
-}
-
-export const addTracks = (newTracks: TrackModel[]): AppThunk<void> => async (dispatch, getState) => {
-  const { tracks } = getState().tracks
-  try {
-    const combinedTracks = tracks.concat(newTracks)
-    const dedup = _.uniqBy(combinedTracks, trackModel => trackModel._id)
-    dispatch(setTracks(dedup))
-  } catch (error) {
-    dispatch(setError(error.message))
-  }
-}
-
-export const subscribeTracks = (): AppThunk<UnsubscribeFn> => (dispatch, getState) => {
+export const playTrack = (track: TrackFieldsFragment): AppThunk<Promise<void>> => async (dispatch, getState) => {
   const { user } = getState().user
   if (user) {
-    const unsubscribeTracksChange = dispatch(subscribeToMessages(Messages.TracksChange, () => dispatch(fetchTracks())))
-
-    return () => unsubscribeTracksChange
-  } else {
-    return () => undefined
-  }
-}
-
-export const playTrack = (track: Track): AppThunk<Promise<void>> => async (dispatch, getState) => {
-  const { user } = getState().user
-  if (user) {
-    return dispatch(sendMessage(Messages.PlayTrack, track)).catch(error => {
-      dispatch(setError(error.message))
-      throw error
+    return dispatch(sendMessage(Messages.PlayTrack, track as Track)).catch(error => {
+      console.error(error.message)
     })
-  } else {
-    dispatch(setError("User not available"))
   }
 }
 
 export const getTrackFromSearchTerm = (searchTerm: string) => {
   return youtubeService.findTracks(searchTerm)
-}
-
-export const updateTrack = (track: TrackModel): AppThunk<Promise<void>> => async (dispatch, getState) => {
-  const updatedTrack = await TrackService.update(track._id, track)
-  dispatch(setTrack(updatedTrack))
 }
