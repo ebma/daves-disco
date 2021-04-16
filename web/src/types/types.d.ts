de67e5b6ff0294e695b18fc496926081b5ddb578
Marcel Ebert <mail@marcel-ebert.de>
2021-04-16 17:04:22 +0200

    Add colorthief dependency

--- web/src/types/types.d.ts
+++ web/src/types/types.d.ts
@@ -1,3 +1,11 @@
 declare module "react-router-transition" {
   export class AnimatedSwitch extends React.Component<any> {}
 }
+
+declare module "colorthief" {
+  type Color = [number, number, number]
+  export type ColorArray = Array<Color>
+  export default class ColorThief {
+    getPalette(image: Element, colorCount?: number, quality?: number): ColorArray
+  }
+}
