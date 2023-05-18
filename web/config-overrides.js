df0dbf25b0e6a272fc89ce8d5270ad5e4519961a
Marcel Ebert <marcel@wolfgang-ebert.de>
2023-05-18 21:05:44 +0200

    Fix webpack build

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,11 +1,31 @@
 const Dotenv = require("dotenv-webpack")
+const webpack = require('webpack');
 
 module.exports = function override(config, env) {
   if (!config.plugins) {
     config.plugins = []
   }
 
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "timers": require.resolve("timers-browserify"),
+    "os": require.resolve("os-browserify"),
+    "url": require.resolve("url"),
+    "vm": require.resolve("vm-browserify"),
+  })
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
+    })
+  ])
+
   config.plugins.push(new Dotenv())
 
-  return config
+  return config;
 }
